f62f8bd380a9bb400b5f52c585710d761d55c16e
rashadharrison <harrisonmartin26@gmail.com>
2021-03-01 14:48:26 -0500

    8kyu

--- 8kyu/app.js
+++ 8kyu/app.js
@@ -150,4 +150,12 @@ function shortenToDate(longDate) {
 
 function getAverage(marks) {
 	return Math.floor(marks.reduce((sum, x) => sum + x) / marks.length);
+
 }
+
+/**Write a function called repeat_str which repeats the given string src exactly count times. */
+const repeatStr = (n, s) => s.repeat(n);
+	
+/**You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0. */
+
+const index = (arr, n) => (n >= arr.length ? -1 : Math.pow(arr[n], n));
