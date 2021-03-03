e1004cf80eadbdce5bb5b6dc2ba8cc06e1d1ca0f
rashadharrison <harrisonmartin26@gmail.com>
2021-03-03 10:22:44 -0500

    added 8kyu

--- 8kyu/app.js
+++ 8kyu/app.js
@@ -159,3 +159,10 @@ const repeatStr = (n, s) => s.repeat(n);
 /**You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0. */
 
 const index = (arr, n) => (n >= arr.length ? -1 : Math.pow(arr[n], n));
+
+
+//The goal is to create a function 'numberToPower(number, power)' that "raises" the number up to power (ie multiplies number by itself power times).
+
+function numberToPower(number, power) {
+	return power > 0 ? number * numberToPower(number, power - 1) : 1;
+}
