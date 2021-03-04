c42ebdbdff429f56f4ed09a47c875c721fc9a900
rashadharrison <harrisonmartin26@gmail.com>
2021-03-03 19:39:28 -0500

    added 8kyu

--- 8kyu/app.js
+++ 8kyu/app.js
@@ -166,3 +166,7 @@ const index = (arr, n) => (n >= arr.length ? -1 : Math.pow(arr[n], n));
 function numberToPower(number, power) {
 	return power > 0 ? number * numberToPower(number, power - 1) : 1;
 }
+
+//Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages. Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+const paperwork = (n, m) => (n < 0 || m < 0 ? 0 : n * m);
