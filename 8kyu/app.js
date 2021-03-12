bb64d130545e01aa0cc711a48debffc19fe31cf2
rashadharrison <harrisonmartin26@gmail.com>
2021-03-11 19:54:19 -0500

    commit

--- 8kyu/app.js
+++ 8kyu/app.js
@@ -255,3 +255,23 @@ const century = year =>
 			.split(' ')
 			.map(name => name[0].toUpperCase())
 			.join('.');
+
+
+//Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+			const countPositivesSumNegatives = arr => {
+				if (arr === null || arr.length < 1) {
+					return [];
+				}
+
+				const newarr = [0, 0];
+
+				arr.forEach(num => {
+					if (num <= 0) {
+						newarr[1] += num;
+					} else {
+						newarr[0] += 1;
+					}
+				});
+
+				return newarr;
+			};
