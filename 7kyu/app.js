ffa71c9b4c922b955de833d440d8dc8431aa3b71
rashadharrison <harrisonmartin26@gmail.com>
2021-04-20 15:06:43 -0400

    7 kyu

--- 7kyu/app.js
+++ 7kyu/app.js
@@ -143,3 +143,9 @@ function highAndLow(numbers) {
 
 //Simple, given a string of words, return the length of the shortest word(s).
 const findShort = s => Math.min(...s.split` `.map(w => w.length)) | 0;
+
+/*Return the number (count) of vowels in the given string.
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+The input string will only consist of lower case letters and/or spaces.*/
+const getCount = str =>
+	Array.from(str).filter(vowel => 'aeiou'.includes(vowel)).length;
--- 8kyu/app.js
+++ 8kyu/app.js
@@ -276,3 +276,7 @@ const divisibleBy = (numbers, divisor) =>
 //Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
 	const twiceAsOld = (dadYearsOld, sonYearsOld) =>
 		Math.abs(sonYearsOld * 2 - dadYearsOld);
+
+
+
+
